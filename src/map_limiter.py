19b21f630de645f93ec3b90c3d8231d0b840a69b
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2022-04-11 15:36:12 +0200

    Add map limiter missing file

---
+++ src/map_limiter.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from dcs.forcedoptions import ForcedOptions
+from dcs.mission import Mission
+
+@dataclass
+class MapLimiter:
+    m: Mission
+
+    def limit_map(self) -> None:
+        m = self.m
+        m.forced_options.options_view = ForcedOptions.Views.OnlyMap
