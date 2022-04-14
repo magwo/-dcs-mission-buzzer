aefd1127b7861e7eebaf73fff66885d98ec216e2
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2022-04-14 21:47:53 +0200

    Add support for excluding groups from drawing

--- src/track_drawer.py
+++ src/track_drawer.py
@@ -20,16 +20,17 @@ class TrackDrawer:
 
     def draw_tracks(self) -> None:
         m = self.m
+        excluded_group_names = self.settings.get("excluded_drawing_group_names", "").split(",")
 
         for coalition_name in m.coalition:
             for country_name in m.coalition[coalition_name].countries:
                 country = m.coalition[coalition_name].countries[country_name]
-
                 for group in country.plane_group:
-                    for plane in group.units:
-                        if len(set(plane.unit_type.tasks) & INCLUDED_TASKS) > 0:
-                            self.draw_track(group, 1, 2)
-                            break
+                    if group.name not in excluded_group_names:
+                        for plane in group.units:
+                            if len(set(plane.unit_type.tasks) & INCLUDED_TASKS) > 0:
+                                self.draw_track(group, 1, 2)
+                                break
 
     def draw_track(self, group: Group, wp1_index: int, wp2_index: int) -> None:
         altitude = Distance(group.points[wp1_index].alt).feet
