3fc48b5591bb72ce42ce35b4df2265af5efac560
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2022-04-25 20:41:51 +0200

    Update script backup

--- scriptsbackup/generate_missions.bat
+++ scriptsbackup/generate_missions.bat
@@ -1,5 +1,6 @@
 :: Note: Use cmd.exe calls so that one failure doesn't stop generation completely
 set "MISSIONS_DIR=C:\Users\DCSServerUser\Saved Games\DCS.openbeta_server\Missions"
+set "SEC_MISSIONS_DIR=C:\Users\DCSServerUser\Saved Games\DCS.openbeta_server_mod\Missions"
 
 cd "C:\Users\DCSServerUser\Documents\dcs-mission-buzzer\"
 
@@ -32,21 +33,9 @@ timeout /t 1
 cmd.exe /c dcs_mission_buzzer.exe --limitmap "%MISSIONS_DIR%\MA_Training_SY.miz" "%MISSIONS_DIR%\MA_Training_SY_LimitedMap_RealisticWX.miz"
 timeout /t 1
 cmd.exe /c dcs_mission_buzzer.exe --donotbuzz --forcenight "%MISSIONS_DIR%\MA_Training_SY.miz" "%MISSIONS_DIR%\MA_Training_SY_Night.miz"
+timeout /t 1
 
-timeout /t 20
+cmd.exe /c xcopy "%MISSIONS_DIR%\MA_Training_CA_*.miz" "%SEC_MISSIONS_DIR%" /y
+cmd.exe /c xcopy "%MISSIONS_DIR%\MA_Training_SY_*.miz" "%SEC_MISSIONS_DIR%" /y
 
-
-::MA_Training_CA_FullMap_ClearWX
-::MA_Training_CA_FullMap_RealisticWX
-
-::MA_Training_CA_LimitedMap_StaticWX
-::MA_Training_CA_LimitedMap_ClearWX
-::MA_Training_CA_LimitedMap_RealisticWX
-
-::MA_Training_SY_FullMap_StaticWX
-::MA_Training_SY_FullMap_ClearWX
-::MA_Training_SY_FullMap_RealisticWX
-
-::MA_Training_SY_LimitedMap_StaticWX
-::MA_Training_SY_LimitedMap_ClearWX
-::MA_Training_SY_LimitedMap_RealisticWX
+timeout /t 5
